217d21b83df3e6c5bb6714ec4c010eb06eb5cebf
shutsuensha <shutsuenshazvezd@gmail.com>
2024-12-17 23:44:05 +0300

    init frontend; vue; tailwindcss; add font;

---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+- vue
+- vue-routes
+- tailwind css
+- flowbite
+- icons
+- spinner
+- axios
+- animation
+- toasts
+
+### Web server
+npm run dev
---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="text-2xl">
+    This text will use the Poppins font globally!
+  </div>
+</template>
