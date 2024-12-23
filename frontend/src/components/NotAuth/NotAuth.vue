0d576a829997e30b580d9f10e75feefff06eba72
shutsuensha <shutsuenshazvezd@gmail.com>
2024-12-23 20:33:38 +0300

    minor improves

--- frontend/src/components/NotAuth/NotAuth.vue
+++ frontend/src/components/NotAuth/NotAuth.vue
@@ -13,24 +13,29 @@ const loading = ref(true)
 const color = ref('red')
 const size = ref('50px')
 
+const textWelcome = ref('Welcome to Our Platform')
+
 const images = ref([])
 
 function changeBgColor1() {
   bg.value = 'bg-red-100'
+  textWelcome.value = 'First time here? - Sign Up'
 }
 
 function changeBgColor2() {
   bg.value = 'bg-green-100'
+  textWelcome.value = 'Already has an account ?'
 }
 
 function changeDefaultColor() {
   bg.value = 'bg-gray-100'
+  textWelcome.value = 'Welcome to Our Platform'
 }
 
 onMounted(async () => {
   initCarousels();
   AOS.init({
-    duration: 1000,  // Длительность анимации
+    duration: 3000,  // Длительность анимации
     once: true,      // Анимация будет воспроизводиться только один раз
   });
   try {
@@ -49,26 +54,27 @@ onMounted(async () => {
 
 <template>
   <ClipLoader v-if="loading" :color="color" :size="size"
-    class="flex items-center justify-center h-screen transition font-extrabold" />
-  <div v-else :class="`${bg} flex items-center justify-center h-screen transition-transform`">
+    class="flex items-center justify-center h-screen font-extrabold" />
+  <div v-else :class="`${bg} flex items-center justify-center h-screen`">
     <div class="items-center space-y-6 flex flex-col">
       <div v-if="bg === 'bg-gray-100'" class="grid grid-cols-5 gap-4">
-        <div v-for="(image, index) in images.slice(0, 5)" :key="index" class="p-2" data-aos="fade-up">
+        <div v-for="(image, index) in images.slice(0, 5)" :key="index" class="p-2"
+          :data-aos="index % 2 === 0 ? 'fade-down' : 'fade-up'">
           <img :src="image" alt="Image" class="w-full h-80 rounded-3xl object-cover">
         </div>
       </div>
       <div v-if="bg === 'bg-red-100'" class="grid grid-cols-5 gap-4">
-        <div v-for="(image, index) in images.slice(5, 10)" :key="index" class="p-2" data-aos="fade-up">
+        <div v-for="(image, index) in images.slice(5, 10)" :key="index" class="p-2" :data-aos="index % 2 !== 0 ? 'fade-down' : 'fade-up'">
           <img :src="image" alt="Image" class="w-full h-80 rounded-3xl object-cover">
         </div>
       </div>
       <div v-if="bg === 'bg-green-100'" class="grid grid-cols-5 gap-4">
-        <div v-for="(image, index) in images.slice(10, 15)" :key="index" class="p-2" data-aos="fade-up">
+        <div v-for="(image, index) in images.slice(10, 15)" :key="index" class="p-2" :data-aos="index % 2 === 0 ? 'fade-down' : 'fade-up'">
           <img :src="image" alt="Image" class="w-full h-80 rounded-3xl object-cover">
         </div>
       </div>
       <div class="text-center">
-        <h1 :class="`text-3xl font-bold text-black mb-6`">Welcome to Our Platform</h1>
+        <h1 class="text-3xl font-bold text-black mb-6">{{ textWelcome }}</h1>
         <div class="space-x-4">
           <!-- Signup Button -->
           <button @mouseenter="changeBgColor1" @mouseleave="changeDefaultColor"
