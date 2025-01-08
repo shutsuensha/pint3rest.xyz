a3390dce7160b0a673db2f46545567862feb6021
shutsuensha <shutsuenshazvezd@gmail.com>
2025-01-08 21:09:15 +0300

    now users can add/delete like in detail pin; view all likes from users + pagination;

---
+++ frontend/src/components/Auth/PinLikesPopover.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const props = defineProps({
+  pin_id: Number
+})
+
+const users = ref([])
+
+const offset = ref(0);
+const limit = ref(5);
+
+const isPinsLoading = ref(false);
+
+async function loadUsers() {
+  if (isPinsLoading.value) {
+    return;
+  }
+
+  isPinsLoading.value = true;
+  try {
+    const response = await axios.get(`/api/likes/pin/likes/${props.pin_id}`, { params: { offset: offset.value, limit: limit.value } })
+    const data = response.data
+    for (let i = 0; i < data.length; i++) {
+      try {
+        const response = await axios.get(`/api/users/user_id/${data[i].user_id}`)
+        const userData = response.data
+        let userImage = null
+        try {
+          const userResponse = await axios.get(`/api/users/upload/${userData.id}`, { responseType: 'blob' });
+          const blobUrl = URL.createObjectURL(userResponse.data);
+          userImage = blobUrl;
+        } catch (error) {
+          console.error(error);
+        }
+        users.value.push({ id: userData.id, username: userData.username, image: userImage })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  } catch (error) {
+    console.error(error)
+  }
+  offset.value += limit.value;
+  isPinsLoading.value = false;
+}
+
+const handleScroll = (event) => {
+  const container = event.target;
+  if (container.scrollTop + container.clientHeight >= container.scrollHeight - 10) {
+    loadUsers();
+  }
+};
+
+onMounted(() => {
+  AOS.init({
+    duration: 500,  // Длительность анимации
+    once: true,      // Анимация будет воспроизводиться только один раз
+  });
+  loadUsers();  // Initial load
+});
+</script>
+
+
+<template>
+  <div @scroll="handleScroll"
+    class="flex flex-col gap-4 bg-white shadow-2xl rounded-xl text-sm font-medium text-gray-700 z-30 h-32 w-60 overflow-y-auto">
+    <RouterLink v-for="user in users" :key="user.id" :to="`/user/${user.username}`" data-aos="fade-up"
+      class="flex items-center space-x-2 hover:underline cursor-pointer">
+      <img :src="user.image" alt="User Image" class="w-10 h-10 rounded-full object-cover" />
+      <span class="text-gray-700 font-medium">{{ user.username }}</span>
+    </RouterLink>
+  </div>
+</template>
--- frontend/src/views/PinView.vue
+++ frontend/src/views/PinView.vue
@@ -1,16 +1,27 @@
 <script setup>
-import { onMounted, ref, watch  } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { useRoute, RouterLink, useRouter } from 'vue-router';
 import axios from 'axios'
 import RelatedPins from '@/components/Auth/RelatedPins.vue';
-
+import PinLikesPopover from '@/components/Auth/PinLikesPopover.vue';
 
 const route = useRoute();
 const pinId = route.params.id
 
 
 
-const pin = ref(null)
+const pin = ref({
+  id: null,
+  user_id: null,
+  title: '',
+  description: '',
+  href: '',
+  image: '',
+  rgb: '',
+  height: '',
+});
+
+
 const pinImage = ref(null)
 const pinImageLoaded = ref(false)
 const pinVideoLoaded = ref(false)
@@ -18,11 +29,21 @@ const pinVideo = ref(null)
 const pinUser = ref(null)
 const pinUserImage = ref(null)
 
-const router = useRouter(); 
+const cntLikes = ref(null)
+const checkUserLike = ref(null)
+
+const router = useRouter();
+
+
+const showPopover = ref(false); // State to control the popover visibility
+
+const insidePopover = ref(false)
+
+const bgSave = ref('bg-red-700')
+const saveText = ref('Save')
 
 
 onMounted(async () => {
-  console.log('pin view')
   try {
     const response = await axios.get(`/api/pins/${pinId}`)
     pin.value = response.data
@@ -54,60 +75,121 @@ onMounted(async () => {
   } catch (error) {
     console.error(error)
   }
+
+  try {
+    const response = await axios.get(`/api/likes/pin/likes/cnt/${pin.value.id}`)
+    cntLikes.value = response.data
+  } catch (error) {
+    console.error(error)
+  }
+
+  try {
+    const response = await axios.get(`/api/likes/pin/user_like/${pin.value.id}`)
+    checkUserLike.value = response.data
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 const goBack = () => {
   router.back();
 };
+
+async function likePin() {
+  if (checkUserLike.value) {
+    try {
+      await axios.delete(`/api/likes/pin/${pin.value.id}`)
+      checkUserLike.value = false
+      cntLikes.value -= 1
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    try {
+      await axios.post(`/api/likes/pin/${pin.value.id}`)
+      checkUserLike.value = true
+      cntLikes.value += 1
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+async function save() {
+  bgSave.value = 'bg-black'
+  saveText.value = 'Saving...'
+  try {
+    const response = await axios.post(`/api/pins/user_saved_pins/${pin.value.id}`, {
+      withCredentials: true
+    })
+    saveText.value = 'Saved'
+
+  } catch (error) {
+    if (error.response.status === 409) {
+      saveText.value = 'U already saved!'
+    }
+  }
+}
 </script>
 
 
 <template>
-  <div class="mt-20 ml-20">
+  <div class="mt-4 ml-20">
     <button @click="goBack" class="absolute top-4 left-20 text-gray-500 ml-20 mt-20 hover:-translate-x-2">
       <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
       </svg>
     </button>
-    <div v-show="pinImageLoaded || pinVideoLoaded" class="grid grid-cols-2 gap-6 mx-60 bg-gray-100 rounded-3xl shadow-lg">
+    <div v-show="pinImageLoaded || pinVideoLoaded"
+      class="grid grid-cols-2 gap-6 mx-60 bg-gray-100 rounded-3xl shadow-lg">
       <!-- Left Column: Image or Video -->
       <div>
-        <img 
-          v-if="pinImage" 
-          :src="pinImage" 
-          alt="Pin Image" 
-          class="h-full w-full rounded-l-3xl"
-          @load="pinImageLoaded = true"
-        />
-        <video 
-          v-if="pinVideo" 
-          :src="pinVideo" 
-          alt="Pin Video" 
-          class="w-full h-auto rounded-l-3xl" 
-          autoplay 
-          loop 
-          muted
-          @loadeddata="pinVideoLoaded = true"
-        />
+        <img v-if="pinImage" :src="pinImage" alt="Pin Image" class="h-auto w-auto rounded-l-3xl"
+          @load="pinImageLoaded = true" />
+        <video v-if="pinVideo" :src="pinVideo" alt="Pin Video" class="w-auto h-auto rounded-l-3xl" autoplay loop muted
+          @loadeddata="pinVideoLoaded = true" />
       </div>
 
       <!-- Right Column: User Information -->
       <div class="flex flex-col">
-        <RouterLink 
-          v-if="pinUser" 
-          :to="`/user/${pinUser.username}`" 
-          class="flex items-center mt-2 hover:underline cursor-pointer"
-        >
-          <img 
-            v-if="pinUserImage" 
-            :src="pinUserImage" 
-            alt="User Profile" 
-            class="w-8 h-8 rounded-full" 
-          />
+        <div class="flex items-center justify-between w-full p-2">
+          <!-- Icon and Likes -->
+          <div class="flex items-center space-x-2">
+            <!-- Icon -->
+            <i @click="likePin" :class="`pi ${checkUserLike ? 'pi-heart-fill' : 'pi-heart'} text-2xl`"></i>
+            <!-- Number of Likes -->
+            <div v-if="cntLikes != 0" class="font-medium text-2xl relative" @mouseover="showPopover = true"
+              @mouseleave="if (!insidePopover) showPopover = false;">
+              <span>{{ cntLikes }}</span>
+              <div v-if="showPopover" @mouseover="insidePopover = true"
+                @mouseleave="insidePopover = false; showPopover = false"
+                class="absolute top-[30px] left-[-50px]">
+                <PinLikesPopover :pin_id="pin.id"/>
+              </div>
+            </div>
+          </div>
+
+          <!-- Save Button -->
+          <button @click="save" :class="`px-6 py-3 text-sm ${bgSave} hover:bg-red-700 text-white rounded-3xl transition`">
+            {{ saveText }}
+          </button>
+        </div>
+        <div v-if="pin.title">
+          {{ pin.title }}
+        </div>
+        <div v-if="pin.description">
+          {{ pin.description }}
+        </div>
+        <div v-if="pin.href">
+          {{ pin.href }}
+        </div>
+        <RouterLink v-if="pinUser" :to="`/user/${pinUser.username}`"
+          class="flex items-center mt-2 hover:underline cursor-pointer">
+          <img v-if="pinUserImage" :src="pinUserImage" alt="User Profile" class="w-8 h-8 rounded-full" />
           <span class="ml-2 text-sm font-medium">@{{ pinUser.username }}</span>
         </RouterLink>
       </div>
     </div>
   </div>
-  <RelatedPins v-if="pinImageLoaded || pinVideoLoaded" :pin_id="pin.id"/>
+  <RelatedPins v-if="pinImageLoaded || pinVideoLoaded" :pin_id="pin.id" />
 </template>
