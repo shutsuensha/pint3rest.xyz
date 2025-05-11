50054f5c626da683f46443a4bc2cc88ca6a5ce6d
shutsuensha <shutsuenshazvezd@gmail.com>
2025-05-11 08:43:59 +0300

    ci/cd

--- nextjs/src/app/resources/content.js
+++ nextjs/src/app/resources/content.js
@@ -56,6 +56,7 @@ const social = [
 
 const home = {
   path: "/",
+  image: "",
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
