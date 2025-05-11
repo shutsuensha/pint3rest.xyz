26670cb8909a9d6bb8a5f954de1047d8d454f687
shutsuensha <shutsuenshazvezd@gmail.com>
2025-05-11 08:21:18 +0300

    ci/cd

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -1,22 +1,23 @@
-// next.config.mjs
 import mdx from "@next/mdx";
 
-const withMDX = mdx({ extension: /\.mdx?$/, options: {} });
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/magic/portfolio",
-  assetPrefix: "/magic/portfolio",
-  trailingSlash: true,
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   sassOptions: {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
   },
+  basePath: "/magic/portfolio",
+  assetPrefix: "/magic/portfolio",
+  trailingSlash: true,
   images: {
-    loader: "default",
-    path: "/magic/portfolio/_next/image",  // ← путь к Image API
+    unoptimized: true, // отключаем серверную оптимизацию изображений
   },
 };
 
